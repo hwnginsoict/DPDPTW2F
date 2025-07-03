8828042e8bbbf048148a791ba1ff55bf2ac4c9b5
hwnginsoict <thoquyenv@gmail.com>
2025-07-04 00:35:45 +0700

    add

--- run_pfg.py
+++ run_pfg.py
@@ -1,6 +1,6 @@
 from utils_new import crossover_operator, mutation_operator, calculate_fitness, create_individual_pickup
-# from utils import create_individual_pickup_lerk, crossover_operator_lerk, mutation_operator_lerk, calculate_fitness_lerk
-# from LERK_utils import crossover_LERK, mutation_LERK, calculate_fitness_LERK, create_individual_LERK
+from utils import create_individual_pickup_lerk, crossover_operator_lerk, mutation_operator_lerk, calculate_fitness_lerk
+from LERK_utils import crossover_LERK, mutation_LERK, calculate_fitness_LERK, create_individual_LERK
 from graph.graph import Graph
 import time
 from moo_algorithm.pfg_moea import run_pfgmoea
@@ -14,32 +14,32 @@ def main(number = 8, type = "LC2", index = 1, seed = 0, num = 100, max_gen = 100
     filepath = f'./data/dpdptw/{number}00/{type}_{number}_{index}.csv'
 
     graph = Graph(filepath)
-    # indi_list_lerk = [create_individual_pickup_lerk(graph) for _ in range(num)]
+    indi_list_lerk = [create_individual_pickup_lerk(graph) for _ in range(num)]
     indi_list = [create_individual_pickup(graph) for _ in range(num)]
-    # indi_list_original = [create_individual_LERK(graph) for _ in range(num)]
+    indi_list_original = [create_individual_LERK(graph) for _ in range(num)]
 
     np.random.seed(seed)
 
-    # start_time_original = time.time()
-    # original_results = run_pfgmoea(4, graph, indi_list_original, num, max_gen, 5, 0.01, crossover_LERK, mutation_LERK, 0.9, 0.1, calculate_fitness_LERK)
-    # original_time = time.time() - start_time_original
-    # original_results["time"] = original_time
+    start_time_original = time.time()
+    original_results = run_pfgmoea(4, graph, indi_list_original, num, max_gen, 5, 0.01, crossover_LERK, mutation_LERK, 0.9, 0.1, calculate_fitness_LERK)
+    original_time = time.time() - start_time_original
+    original_results["time"] = original_time
 
     start_time_proposed = time.time()
     proposed_results = run_pfgmoea(4, graph, indi_list, num, max_gen, 5, 0.01, crossover_operator, mutation_operator, 0.9, 0.1, calculate_fitness)
     proposed_time = time.time() - start_time_proposed
     proposed_results["time"] = proposed_time
 
-    # start_time_pfg = time.time()
-    # pfg_results = run_pfgmoea(4, graph, indi_list_lerk, num, max_gen, 5, 0.01, crossover_operator_lerk, mutation_operator_lerk, 0.9, 0.1, calculate_fitness_lerk)
-    # pfg_time = time.time() - start_time_pfg
-    # pfg_results["time"] = pfg_time
+    start_time_pfg = time.time()
+    pfg_results = run_pfgmoea(4, graph, indi_list_lerk, num, max_gen, 5, 0.01, crossover_operator_lerk, mutation_operator_lerk, 0.9, 0.1, calculate_fitness_lerk)
+    pfg_time = time.time() - start_time_pfg
+    pfg_results["time"] = pfg_time
     
     # 4) Combine and store both results in the same JSON
     final_results = {
-        # "Original": original_results,
+        "Original": original_results,
         "Proposed": proposed_results,
-        # "PFG-EMOA": pfg_results,
+        "PFG-EMOA": pfg_results,
     }
 
     name_store = f"Result/{type}_{number}_{index}_{seed}.json"
